ae2d9cbb3c64c31666732a021e5ff0ec9f412e07
Shirobokov-aa <136621836+Shirobokov-aa@users.noreply.github.com>
2023-12-27 21:13:27 +0300

    process

--- yakor.vue/src/App.vue
+++ yakor.vue/src/App.vue
@@ -1,16 +1,17 @@
 <script setup>
 import headerTop from './components/headerTop.vue'
-
+import headerBottom from './components/headerBottom.vue'
+import siteMap from './components/siteMap.vue'
 </script>
 
 <template>
   <header>
     <headerTop></headerTop>
+    <headerBottom></headerBottom>
+    <siteMap></siteMap>
   </header>
 
-  <main>
-
-  </main>
+  <main></main>
 </template>
 
 <style scoped></style>
